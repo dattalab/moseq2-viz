29e1194ec851d10455b10d4e72d6ce5b4f301992
Ayman Zeine <a.zeine.96@gmail.com>
2020-04-17 11:11:26 -0400

    install fix: readded setup.py requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(
     version='0.3.1',
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=[],
+    install_requires=['six', 'h5py', 'tqdm', 'scipy', 'numpy', 'scipy',
+                      'psutil', 'click', 'psutil', 'future', 'scikit-learn',
+                      'scikit-image', 'joblib', 'seaborn', 'cytoolz', 'networkx',
+                      'ipywidgets', 'matplotlib', 'statsmodels', 'hdf5storage',
+                      'ruamel.yaml', 'dtaidistance', 'opencv-python'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )
