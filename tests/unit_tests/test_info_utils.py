6665fa0d6e71b71b3f414996bdb5edaa6a30f118
Ayman Zeine <a.zeine.96@gmail.com>
2020-08-06 12:47:29 -0400

    Updated entropy/entropy rate tests

--- tests/unit_tests/test_info_utils.py
+++ tests/unit_tests/test_info_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from unittest import TestCase
 from moseq2_viz.model.util import parse_model_results
 from moseq2_viz.info.util import entropy, entropy_rate
-from moseq2_viz.model.util import get_syllable_statistics, get_transition_matrix, relabel_by_usage
+from moseq2_viz.model.util import get_syllable_statistics, get_transition_matrix
 
 
 class TestInfoUtils(TestCase):
@@ -47,11 +47,10 @@ class TestInfoUtils(TestCase):
 
         model_data = parse_model_results(joblib.load(model_fit))
         labels = model_data['labels']
-        truncate_syllable = 40
+        truncate_syllable = 100
         smoothing = 1.0
         tm_smoothing = 1.0
-        normalize = 'bigram'
-        ent_r = []
+
         for v in labels:
 
             usages = get_syllable_statistics([v])[0]
@@ -63,31 +62,33 @@ class TestInfoUtils(TestCase):
             else:
                 truncate_point = truncate_point[0]
 
-            syllables = syllables[:truncate_point]
-
             usages = np.array(list(usages.values()), dtype='float')
             usages = usages[:truncate_point] + smoothing
             usages /= usages.sum()
 
-            tm = get_transition_matrix([v],
-                                       max_syllable=100,
-                                       normalize='none',
-                                       smoothing=0.0,
+            for norm in ['none', 'bigram', 'rows', 'columns']:
 
-                                       disable_output=True)[0] + tm_smoothing
-            tm = tm[:truncate_point, :truncate_point]
+                tm = get_transition_matrix([v],
+                                           max_syllable=100,
+                                           normalize='none',
+                                           smoothing=0.0,
+                                           disable_output=True)[0] + tm_smoothing
+                tm = tm[:truncate_point, :truncate_point]
 
-            assert tm.shape == (truncate_point, truncate_point)
+                assert tm.shape == (truncate_point, truncate_point)
 
-            if normalize == 'bigram':
-                tm /= tm.sum()
-            elif normalize == 'rows':
-                tm /= tm.sum(axis=1, keepdims=True)
-            elif normalize == 'columns':
-                tm /= tm.sum(axis=0, keepdims=True)
+                if norm == 'bigram':
+                    tm /= tm.sum()
+                elif norm == 'rows':
+                    tm /= tm.sum(axis=1, keepdims=True)
+                elif norm == 'columns':
+                    tm /= tm.sum(axis=0, keepdims=True)
 
-            ent_r.append(-np.sum(usages[:, None] * tm * np.log2(tm)))
+                real_er = -np.sum(usages[:, None] * tm * np.log2(tm))
 
-        test_ent_r = entropy_rate(labels)
-        assert len(test_ent_r) == 2  # for 2 sessions in modeling
-        np.testing.assert_almost_equal(ent_r, test_ent_r, 2)
+                test_er = entropy_rate(labels, normalize=norm, truncate_syllable=truncate_syllable,
+                                       smoothing=smoothing, tm_smoothing=tm_smoothing)
+
+                assert len(test_er) == 2  # for 2 sessions in modeling
+
+                np.testing.assert_allclose(real_er, test_er, rtol=1e-2)
